3bfdf2364a1717dea986f567727ba418845fe4da
himanshu007-creator <addyjeridiq@gmail.com>
2025-02-20 08:42:54 +0530

    OMRZen hickups fixed 🚀

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -181,7 +181,7 @@ export default function TestPage() {
               onClick={handleSubmit}
               disabled={Object.keys(answers).length === 0}
             >
-              Submit Test
+              Submit and Check
             </Button>
           </motion.div>
         </main>
